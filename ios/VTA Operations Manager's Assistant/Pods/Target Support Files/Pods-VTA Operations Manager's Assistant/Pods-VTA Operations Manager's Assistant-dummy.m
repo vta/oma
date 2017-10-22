e0c8fb19606f7996b7b11a59f9b43027930450ae
nwalker2398 <nwalker2398@yahoo.com>
2017-10-22 15:13:15 -0700

    Added Google Sheets API Libraries to ios project.

--- ios/VTA Operations Manager's Assistant/Pods/Target Support Files/Pods-VTA Operations Manager's Assistant/Pods-VTA Operations Manager's Assistant-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VTA_Operations_Manager_s_Assistant : NSObject
-@end
-@implementation PodsDummy_Pods_VTA_Operations_Manager_s_Assistant
-@end
